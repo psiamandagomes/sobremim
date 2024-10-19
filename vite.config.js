df74bd2640d4f803dea68bd88bc4cc25cfa9097d
alexandre-silvaa <alexandrebsilva9@gmail.com>
2024-10-18 21:22:44 -0300

    ci: 💻 github pages

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   plugins: [react()],
+  base: "/psiamanda_landingpage",
   server: {
     host: '0.0.0.0'
   }
